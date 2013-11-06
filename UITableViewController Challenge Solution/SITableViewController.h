1f29999878e0ce98f229a2e51a42772f163e4404
Jordan Stiver <jordan.stiver@gmail.com>
2013-11-05 22:37:52 -0500

    Solved.

---
+++ UITableViewController Challenge Solution/SITableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SITableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Jordan Stiver on 11/5/13.
+//  Copyright (c) 2013 Jordan Stiver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SITableViewController : UITableViewController
+
+@end
