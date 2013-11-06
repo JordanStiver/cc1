2bda614b13b0cc49679de0530e70cf1e0360687c
Jordan Stiver <jordan.stiver@gmail.com>
2013-11-05 21:15:15 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/SIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SIAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Jordan Stiver on 11/5/13.
+//  Copyright (c) 2013 Jordan Stiver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Jordan Stiver on 11/5/13.
+//  Copyright (c) 2013 Jordan Stiver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SIAppDelegate class]));
+    }
+}
